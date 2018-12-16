3126509cd7b51d3222682e2ef7354aab090b2a17
oonderstanbable <45098109+oonderstanbable@users.noreply.github.com>
2018-12-16 00:19:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  if (cmd === `{prefix} what is the weather like`){
+  if (cmd === `${prefix} what is the weather like`){
     message.channel.send("https://www.google.com/search?q=world+weather&rlz=1CAASUJ_enUS823&oq=world+weather&aqs=chrome..69i57j0j69i60j0l3.4051j1j4&sourceid=chrome&ie=UTF-8&safe=active&ssui=on");
   }
 });
