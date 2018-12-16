3f33ae8ff125dab66d06305aa4c17c28041912f7
oonderstanbable <45098109+oonderstanbable@users.noreply.github.com>
2018-12-15 22:59:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`Hey, Wassup!`);
+  bot.user.setActivity(`Google Home is at your service!`);
 });
 
 bot.on("message", async message => {
@@ -16,9 +16,9 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  if (cmd === `${prefix}ping`){
-    message.channel.send("Pong!");
+  if (cmd === `ok google{prefix}what is the weather like`){
+    message.channel.send("https://www.google.com/search?q=world+weather&rlz=1CAASUJ_enUS823&oq=world+weather&aqs=chrome..69i57j0j69i60j0l3.4051j1j4&sourceid=chrome&ie=UTF-8&safe=active&ssui=on");
   }
 });
 
-bot.login(process.env.token);
+bot.login(process.env.NTIzNTI2MDI2NzE0Njc3MjUz.DvdsUg.VefjpGPO5N-LLXZ2L-bws2qGDHY);
